f45f4ca8d50b79db37e683b96b973bc0dfe4c305
martinwolcansp <martin.wolcan@spseguridad.com.ar>
2026-01-31 08:45:25 -0300

    Segunda etapa GHL

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ app = FastAPI()
 @app.post("/webhook/opportunity")
 async def receive_opportunity(request: Request):
     payload = await request.json()
+
     print("🔥 Webhook recibido desde NetSuite")
     print(payload)
+
+    ghl_payload = {
+        "contactId": payload.get("ghl_contact_id"),
+        "name": payload.get("netsuite_title"),
+        "pipelineId": "PIPELINE_ID",
+        "stageId": "STAGE_ID",
+        "status": "open"
+    }
+
+    print("🚀 Payload a enviar a GHL")
+    print(ghl_payload)
+
     return {"status": "ok"}
