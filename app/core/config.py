5a46d955cf720c39f56d7d95739831194418784d
martinwolcansp <martin.wolcan@spseguridad.com.ar>
2026-04-29 16:09:33 -0300

    modificacion de logs

--- app/core/config.py
+++ app/core/config.py
@@ -5,10 +5,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# =========================
+# GHL CORE
+# =========================
 GHL_API_KEY = os.getenv("GHL_API_KEY")
 GHL_LOCATION_ID = os.getenv("LOCATION_ID")
 
 PIPELINE_ID = os.getenv("PIPELINE_ID")
 PIPELINE_STAGE_ID = os.getenv("PIPELINE_STAGE_ID")
 
-CUSTOM_FIELD_NETSUITE_OPPORTUNITY_ID = os.getenv("NETSUITE_OPP_CF_ID")
+# =========================
+# CUSTOM FIELDS
+# =========================
+CUSTOM_FIELD_NETSUITE_OPPORTUNITY_ID = os.getenv("CUSTOM_FIELD_NETSUITE_OPPORTUNITY_ID")
